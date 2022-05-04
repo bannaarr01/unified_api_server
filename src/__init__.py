e25e3664676ae3711ec74d42d3c2f5b74be1ba69
bannaarr01 <tbannaarr@gmail.com>
2022-05-05 01:08:46 +0800

    cache n refactoring ⚙

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 import os
-from src.shippingrate import shippingrate
+from src.shippingrate import shippingrate, cache
 from src.auth import auth
 from src.database import db
 from flask_jwt_extended import JWTManager
@@ -12,26 +12,31 @@ def create_app(test_config=None):
 
     if test_config is None:
          app.config.from_mapping(
-            SECRET_KEY=os.environ.get('SECRET_KEY'),
-            SQLALCHEMY_DB_URI=os.environ.get('SQLALCHEMY_DB_URI'),
+            SECRET_KEY=os.environ.get('SECRET_KEY', default=False),
+            SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DB_URI', default=False),
             SQLALCHEMY_TRACK_MODIFICATIONS=False,
-            JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY'),
+            JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY', default=False),
             SWAGGER={
                  'title': 'Unified API Server', 
                  'uiversion': 3
-            }
+            },
+            CACHE_TYPE=os.environ.get('CACHE_TYPE', default=False),
+            CACHE_REDIS_HOST=os.environ.get('CACHE_REDIS_HOST', default=False),
+            CACHE_REDIS_PORT=os.environ.get('CACHE_REDIS_PORT', default=False),
+            CACHE_REDIS_DB=os.environ.get('CACHE_REDIS_DB', default=False),
+            CACHE_REDIS_URL=os.environ.get('CACHE_REDIS_URL', default=False),
+            CACHE_DEFAULT_TIMEOUT=os.environ.get('CACHE_DEFAULT_TIMEOUT', default=False)
          )
     else:
          app.config.from_mapping(test_config)
 
     db.app = app
     db.init_app(app)
+    cache.init_app(app)
     JWTManager(app)
     db.create_all()
-
     app.register_blueprint(auth)
-    app.register_blueprint(shippingrate)   
-
+    app.register_blueprint(shippingrate)
     Swagger(app, config=swagger_config, template=template)
 
     #error handling
--- src/shippingrate.py
+++ src/shippingrate.py
@@ -8,30 +8,116 @@ from concurrent.futures import ThreadPoolExecutor
 from flask_jwt_extended import jwt_required
 import validators
 from flasgger import swag_from
+from flask_caching import Cache
+
+
+cache = Cache()
+# cache = Cache(config={'CACHE_TYPE': 'SimpleCache'})
 
 shippingrate = Blueprint('shippingrate', __name__, url_prefix='/api/v1/shippingrate')
 
-#get Random USER AGENT
-def getRandomUA():
+session = requests.Session()
+
+#get Random USER AGENT and Header
+def get_header():
     relatives = ['chrome', 'firefox', 'opera','safari']
     ua = UserAgent(family=random.shuffle(relatives)) #shuffle UA family
-    return ua.random().strip()
+    header = { 'accept': 'application/json, text/javascript, */*; q=0.01',
+                    'User-Agent': ua.random().strip()}  
+    return header
 
+#get jtexpress hidden token
 def parseJt():
     session = requests.Session()
-    jt_req = session.get('https://www.jtexpress.my/shipping-rates', verify = False)
-    tree = html.fromstring(jt_req.content)
-    form = tree.find('.//form')
-    token = form.find('.//input[@name="_token"]').value
-    parsedList = [token, jt_req.cookies]
-    return parsedList
+    try:
+        jt_req = session.get('https://www.jtexpress.my/shipping-rates', verify = False, timeout=5)
+        tree = html.fromstring(jt_req.content)
+        form = tree.find('.//form')
+        token = form.find('.//input[@name="_token"]').value
+        parsedList = [token, jt_req.cookies]
+        return parsedList
+    except Exception as e:
+        return 'error'
 
-def urls(arg:tuple):
-    return arg
+
+################## ############ ####### #########
+def jt_express(origin_postcode, destination_postcode, length, width, height, weight):
+    parsedList = parseJt()
+    if parsedList != 'error':
+        payload = { '_token': str(parsedList[0]),
+                        'shipping_rates_type': 'domestic',
+                        'sender_postcode': int(origin_postcode),
+                        'receiver_postcode': int(destination_postcode),
+                        'destination_country': 'BWN',
+                        'shipping_type': 'EX',
+                        'weight': int(weight),
+                        'length': int(length),
+                        'width': int(width),
+                        'height': int(height),
+                        'item_value': ''
+                        } 
+        headers = get_header()  
+        try:
+            req = session.post(url="https://www.jtexpress.my/shipping-rates", params=payload, verify=False, headers=headers,cookies=parsedList[1])
+
+            tree = html.fromstring(req.content)
+            table = tree.xpath("//table[@class='table table-bordered mb-0']//td[@class='col-4']/text()")[6].strip()
+
+            return {"courier": "jtexpress", "rate":float(table)}
+        except Exception as e:
+            return 'error' 
+    else:
+        return 'error' 
 
 
+###### ############ ############ ######
+def line_clear_express(origin_postcode, destination_postcode, weight):
+    payload = { 
+                    'zipfrom': origin_postcode,
+                    'zipto': destination_postcode,
+                    'weight': int(weight),
+                    'parceltype':'P'
+                    }
+    headers = get_header()   
+    try:                                   
+        req=session.get('https://lineclearexpress.com/my/quote/quote.php', params=payload, headers=headers, verify = False)
+        data = req.json()
+        return {"courier": "lineclearexpress", "rate":float(data["price"])}
+    except Exception as e:
+        return 'error' 
+
+
+##################### ######### ######
+def city_link(origin_postcode, destination_postcode, length, width, height, weight):
+
+    headers = get_header()                   
+    payload = {'origin_country': 'MY',
+                'origin_state': 'Selangor', #Invalid but required
+                'origin_postcode': int(origin_postcode),
+                'destination_country': 'MY',
+                'destination_state': 'Negeri Sembilan', #Invalid but required
+                'destination_postcode': int(destination_postcode),
+                'length': int(length),
+                'width': int(width),
+                'height': int(height),
+                'selected_type': 1,
+                'parcel_weight': weight,
+                'document_weight': ''
+                }
+    try:            
+        req = session.post('https://www.citylinkexpress.com/wp-json/wp/v2/getShippingRate',headers=headers, params=payload)
+    
+        result = json.loads(req.text)
+        for key in result:
+            value = result[key]["data"]["rate"]
+            return {"courier": "citylink", "rate":float(value)}
+    except Exception as e:
+        return 'error'
+
+########################################################################
 @shippingrate.post('/')
 @jwt_required()
+@cache.cached(timeout=10, key_prefix="dataList", query_string=True)
 @swag_from('./docs/shippingrate/shippingrate.yaml')
 def multi_shipping_rates():
         origin_postcode = request.get_json().get('origin_postcode', '')
@@ -41,7 +127,7 @@ def multi_shipping_rates():
         height = request.get_json().get('height', '')
         weight = request.get_json().get('weight', '')
 
-
+        #input validations
         if not validators.length(origin_postcode, min=5, max=5):
             return jsonify({'error': 'Enter valid origin post code'}), 422
 
@@ -60,67 +146,16 @@ def multi_shipping_rates():
         if not validators.between(int(weight), min=1, max=999):
             return jsonify({'error': 'Enter valid weight (kg)'}), 422   
 
-        parsedList = parseJt()
+        #retrieve data
+        dataList = []
+        citylink = city_link(origin_postcode, destination_postcode, length, width, height, weight)
+        if citylink != 'error': #cityLink
+            dataList.append(citylink)
+        jtexpress = jt_express(origin_postcode, destination_postcode, length, width, height, weight)
+        if jtexpress != 'error': #jtExpress
+            dataList.append(jtexpress)
+        lineclearexpress = line_clear_express(origin_postcode,destination_postcode,weight)
+        if lineclearexpress != 'error': #lineclearexpress
+            dataList.append(lineclearexpress)
 
-        headers = { 'accept': 'application/json, text/javascript, */*; q=0.01',
-                    'User-Agent': getRandomUA()}
-
-        ctl_payload = {'origin_country': 'MY',
-                    'origin_state': 'Selangor',#Invalid but required
-                    'origin_postcode': int(origin_postcode),
-                    'destination_country': 'MY',
-                    'destination_state': 'Selangor',#Invalid but required
-                    'destination_postcode': int(destination_postcode),
-                    'length': int(length),
-                    'width': int(width),
-                    'height': int(height),
-                    'selected_type': 1,
-                    'parcel_weight': int(weight),
-                    'document_weight': ''
-                    }
-        jt_payload = { '_token': str(parsedList[0]),
-                    'shipping_rates_type': 'domestic',
-                    'sender_postcode': int(origin_postcode),
-                    'receiver_postcode': int(destination_postcode),
-                    'destination_country': 'BWN',
-                    'shipping_type': 'EX',
-                    'weight': int(weight),
-                    'length': int(length),
-                    'width': int(width),
-                    'height': int(height),
-                    'item_value': ''
-                    } 
-        lc_payload = { 
-                    'zipfrom': origin_postcode,
-                    'zipto': destination_postcode,
-                    'weight': int(weight),
-                    'parceltype':'P'
-                    }          
-        #Session object to increase performance          
-        session = requests.Session()
-
-        list_of_urls = [
-        session.post(url="https://www.citylinkexpress.com/wp-json/wp/v2/getShippingRate",  params=ctl_payload, headers=headers, verify=False),
-        session.post(url="https://www.jtexpress.my/shipping-rates", params=jt_payload, verify=False, headers=headers,cookies=parsedList[1]),
-        session.get('https://lineclearexpress.com/my/quote/quote.php', params=lc_payload, headers=headers, verify = False)
-            ] 
-
-        with ThreadPoolExecutor(max_workers=3) as pool:
-            response_list =  list(pool.map(urls,list_of_urls))
-        dataList = []    
-        i = 0
-        while i < len(response_list):
-            if(i==0): #cityLink
-                result = json.loads(response_list[i].text)
-                for key in result:
-                    value = result[key]["data"]["rate"]
-                    dataList.append({"courier": "citylink", "rate":float(value)})
-            elif(i==1): #jtexpress
-                tree = html.fromstring(response_list[i].content)
-                table = tree.xpath("//table[@class='table table-bordered mb-0']//td[@class='col-4']/text()")[6].strip()
-                dataList.append({"courier": "jtexpress", "rate":float(table)})
-            elif(i==2): #clearlineexpress    
-                data = response_list[i].json()
-                dataList.append({"courier": "lineclearexpress", "rate":float(data["price"])})     
-            i += 1
-        return jsonify({"data":dataList}), 200
+        return jsonify({'data': dataList})
